1b5a305de5c2f5694c3c1c45dbf313202872cbad
[David Butler] <[dbutler4@hawk.iit.edu]>
2017-10-10 12:01:28 -0500

    Added constructor for gameBoard and updateBoard(self, character, x, y)

--- gameLogic.py
+++ gameLogic.py
@@ -4,6 +4,17 @@ Created on Tue Oct 10 11:25:15 2017
 
 Holds the various functions for processsing the game
 
-@author: Isaac Galang
+@author: David Butler
 """
+class gameBoard:
+	board = [[' ', ' ', ' '],[' ', ' ', ' '],[' ', ' ', ' ']]
+
+	def __init__(self):
+		self.board = board
+
+	def updateBoard(self, character, x, y):
+		self.board[x][y] = character
+		checkWin(self, character, x, y)
+
+	#def checkWin(self, charater, x, y):
 
